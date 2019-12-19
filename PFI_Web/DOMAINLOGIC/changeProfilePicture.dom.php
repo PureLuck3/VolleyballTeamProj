dcdf99751a1fae1d3626af470f0f6a7a5662e138
NvQcFox <58307375+NvQcFox@users.noreply.github.com>
2019-12-19 12:54:45 -0500

    Add files via upload

---
+++ PFI_Web/DOMAINLOGIC/changeProfilePicture.dom.php
@@ -0,0 +1,39 @@
+<?php
+
+include "../CLASSES/MEDIA/media.php";
+
+session_start();
+
+if(isset($_FILES['Media']))
+{
+ 
+ $target_dir = "MEDIAS/" . $_SESSION["userID"] . "/";
+
+ //obtenir l'extention du fichier uploader
+ $media_file_type = pathinfo($_FILES['Media']['name'] ,PATHINFO_EXTENSION);
+ //$media_file_ext = strtolower(end(explode('.',$_FILES['Media']['name'])));
+
+ $img_extensions_arr = array("jpg","jpeg","png","gif");
+
+ if(in_array($media_file_type, $img_extensions_arr)){
+     $type = "image";
+     echo "image";
+ }
+ else{
+     echo "INVALID FILE TYPE";
+     die();
+ }
+
+ $file_name = "profilePicture.jpg";
+ 
+ //creation de l'url pour la DB
+ $url = $target_dir . $file_name;
+ 
+ //deplacement du fichier uploader vers le bon repertoire (MEDIAS)
+ move_uploaded_file($_FILES['Media']['tmp_name'], "../" . $url);
+
+
+ //redirection
+ header("Location: ../HTML/myprofileview.php");
+ die();
+}
