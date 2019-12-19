9480e5b6cd47375abe07dd2eb5fb2a81b4ea8a08
NvQcFox <58307375+NvQcFox@users.noreply.github.com>
2019-12-19 18:25:51 -0500

    Add files via upload

--- PFI_Web/HTML/mediaTemplate.php
+++ PFI_Web/HTML/mediaTemplate.php
@@ -1,15 +1,20 @@
 <div class="Container">
-    <h2><?php echo $title?></h2>
     <?php 
         $ext = pathinfo($url ,PATHINFO_EXTENSION);
 
-        if($type == "image"){
-            echo "<img width = '320' height='320' src='$url' alt='$title#$id'>";
-        }
-        else if($type == "video"){
-            echo "<video width='320' height='240' controls>
-            <source src='$url' type='video/$ext'>
-            </video>";
-        }
+        echo "<div class='card' style='width:400px'>
+                <div class='card-header'>
+                <a href=''>$title </a>" . $description . " " . $date . 
+                "</div>
+                <div class='card-body'>";
+                    if($type == "image"){
+                        echo "<img width = '320' height='320' src='$url' alt='$title#$id'>";
+                    }
+                    else if($type == "video"){
+                        echo "<video width='320' height='240' controls>
+                        <source src='$url' type='video/$ext'>
+                        </video>";
+                    }
+                echo "</div>";
+            echo "</div>";   
     ?>
-</div>
