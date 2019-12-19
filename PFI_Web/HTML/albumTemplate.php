4f267437f68794c37674c0791ebd0b496748b77c
NvQcFox <58307375+NvQcFox@users.noreply.github.com>
2019-12-19 18:32:55 -0500

    Add files via upload

---
+++ PFI_Web/HTML/albumTemplate.php
@@ -0,0 +1,8 @@
+<div class="Container">
+    <?php
+        echo "<div class='card' style='width:400px'>
+                <div class='card-header'>
+                    <a href=''>$title </a>" . "  " . $user->get_username() . "'s album " . " " . $date . 
+                "</div>
+            </div>";
+    ?>
--- PFI_Web/HTML/albumview.php
+++ PFI_Web/HTML/albumview.php
@@ -2,7 +2,8 @@
     $title= "MY ALBUMS";
     include "../UTILS/sessionhandler.php";
     include_once __DIR__ . "/../DOMAINLOGIC/album.dom.php";
-    require "header.php"; 
+    require "header.php";
+    include_once "../CLASSES/ALBUM/album.php";
 
     if(!validate_session())
     {
@@ -16,19 +17,26 @@
 
 <div class="container" style="margin-top:30px">
     <div class="row">
-        <div class="col-sm-4">  
-            <div class="Container" style="width:400px">
-                
-                <h2><?php echo $name ."'s" . " Albums" ?></h2>
-                    <div class='card' style='width:400px'>
-                        <div class='card-header'>
-                            <a href=''><?php  ?></a>
-                        </div>
-                        <div class='card-body'>
-                            <?php display_Albums()?>
-                        </div>
-                    </div>
+        <div class="col-sm-8">  
+            <div class="Container" style="width:400px">               
+            <?php
+                 echo "<h2>$name" . "s'" . " Albums" . "</h2>";
+                 $array = array();
+                 $albums = Album::get_all_album_by_userID($_SESSION["userID"]);
+ 
+                foreach($albums as $a)
+                {
+                    array_push($array, $a);
+                }
+                foreach($array as $elem)
+                {
+                    $elem->display();
+                }
+            ?>               
             </div>
+        </div>
+        <div class="col-sm-4">
+            <?php //include "uploadalbumview.php";?>
         </div>
     </div>
 </div>
