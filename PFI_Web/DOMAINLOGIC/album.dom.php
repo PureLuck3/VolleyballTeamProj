a027062eb18c7414004bd7dcf317133ca4da4e83
Jogagnon20 <58305873+Jogagnon20@users.noreply.github.com>
2019-12-19 14:46:03 -0500

    Add files via upload

--- PFI_Web/DOMAINLOGIC/album.dom.php
+++ PFI_Web/DOMAINLOGIC/album.dom.php
@@ -5,17 +5,16 @@
     {
         $array = array();
 
-        $albums = ALBUM::get_all_album();
+        $albums = ALBUM::get_all_album_by_userID($_SESSION["userID"]);
 
         foreach($albums as $a)
         {
-            $array->array_push($a);
+            array_push($array, $a);
         }
 
         foreach($array as $elems)
         {
             $elems->display();                
         }
-        
     }
 ?>
