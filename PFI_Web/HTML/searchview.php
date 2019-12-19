9c2422c69e46aadcd2db02f08559b1371751f43c
Jogagnon20 <58305873+Jogagnon20@users.noreply.github.com>
2019-12-19 18:18:56 -0500

    Add files via upload

--- PFI_Web/HTML/searchview.php
+++ PFI_Web/HTML/searchview.php
@@ -1,18 +1,23 @@
 <?php
-    $title= "Billboard";
+
+    include "../CLASSES/ALBUM/album.php";
     require "header.php";
-    include "../DOMAINLOGIC/search.dom.php";
-    $searchResult = $_POST["searchResult"];
+    $album_list = Album::search_album($_GET["search"]);
 ?>
 
+<h1 class="my-4">Résultat Recherche : </h1>
 <div class="container" style="margin-top:30px">
-
-        <div class="row">
-            <div class="col-sm-8">
-            <?php search_result($searchResult); ?>
-            </div>
+    <div class="row">
+        <div class="col-sm-8">
+            <?php  
+                foreach($album_list as $album)
+                {
+                    $album->display();
+                }
+            ?>
         </div>
     </div>
+</div>
 
 <?php
     require "footer.php";
